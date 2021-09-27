4287273814803386038fc6001cd2a0a93d3fa050
ahmadshaheer1 <ashaheerki@gmail.com>
2021-09-27 11:39:31 +0430

    styled components added

---
+++ pages/feed.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import  styled  from 'styled-components';
+
+const feed = () => {
+    return (
+        <Wrapper>
+            sdf
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    background: #09f;
+    color: #fff;
+    padding: 1rem .5rem;
+`
+
+export default feed
